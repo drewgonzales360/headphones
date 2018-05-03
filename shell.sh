d1392c73d4bc8ebc9dd7b8c7d153cbbadfa95ca5
Drew Gonzales <drewgonzales360@gmail.com>
2018-05-03 16:19:46 -0700

    shell rewrite, untested

--- shell.sh
+++ shell.sh
@@ -1,35 +1,95 @@
 #!/usr/bin/env zsh
+################################################################################
+# I use this shell script to connect my headphones to my desktop.
+################################################################################
 
-# Connect headphones
-rfkill unblock bluetooth          # allows you to power on the bluetooth radio
-sleep 2                           # wait for the
-coproc bluetoothctl               # start non interactive use with blootoothctl
-echo 'power on\n' >&p             # turn on bluetooth radio
-exec 4<&p
-echo  'connect E9:08:EF:56:12:64\n' >&p # connect mac address of my headphone
-exec 5<&p
-echo  'exit\n' >&p
-exec 6<&p
-coproc :
-cat <&4
-cat <&5
-cat <&6
+################################################################################
+# This function takes one parameter and connects as an a2dp sink
+# @param $1 mac address seprated with colons E9:08:EF:56:12:64
+function connect_bluetooth() {
+	rfkill unblock bluetooth          # allows you to power on the bluetooth radio
+	sleep 1                           # wait for the
+	coproc bluetoothctl               # start non interactive use with blootoothctl
+	echo 'power on\n' >&p             # turn on bluetooth radio
+	exec 4<&p
+	echo 'connect $1\n' >&p           # connect mac address of my headphone
+	exec 5<&p
+	echo 'exit\n' >&p
+	exec 6<&p
+	coproc :
+	cat <&4
+	cat <&5
+	cat <&6
+	# allow the card to be seen as a sink
+	pactl set-card-profile bluez_card.$(echo $1 | sed 's/:/_/g') a2dp_sink
+}
+################################################################################
+
+################################################################################
+# This function will take one parameter and set it as the default sink, then
+# start streaming all audio to it.
+# @param $1 bluez_sink.E9_08_EF_56_12_64
+function set-sink() {
+	# set the default sink to the bluez sink
+	pacmd set-default-sink $1
+
+	# Move all currently playing audio streams to sink
+	# List all current streams
+	SINKS=($(pacmd list-sink-inputs | grep "index:" | awk  -F" " '{print $2}'))
+
+	# Move all current streams to my sink
+	for i in ${SINKS[@]}; do
+		pacmd move-sink-input ${i} $1
+	done
+}
+################################################################################
+
+################################################################################
+# connect headphones and use it as default sound sink
+# @param $1 mac address seprated with colons E9:08:EF:56:12:64
+function use_mpow() {
+	mpow_mac_address=E9:08:EF:56:12:64
+	connect_bluetooth $mpow_mac_address
+	set-sink $(bluez_sink.$(echo $mpow_mac_address | sed 's/:/_/g'))
+}
+
+function use_speakers() {
+	set-sink alsa.cis.sink
+}
+################################################################################
+
+usage="$(basename "$0") [SINK] [-h] -- connect bluetooth headphones
+
+where:
+		-h  show this help text
+
+available cards:
+$(pactl list cards short)
+"
+
+while getopts ':h' option; do
+	case "$option" in
+		h) echo "$usage"
+			exit
+			;;
+		\?) printf "illegal option: -%s\n" "$OPTARG" >&2
+			echo "$usage" >&2
+			exit 1
+			;;
+	esac
+done
+shift $((OPTIND - 1))
 
 
-# use this to find the card.
-pactl list cards short
+if [[ $# -lt 2 ]]; then
+	use_mpow
+	return
+elif [[ $# -gt 2 ]]; then
+	echo "$usage"
+fi
 
-# allow the card to be seen as a sink
-pactl set-card-profile bluez_card.E9_08_EF_56_12_64 a2dp_sink
-
-# set the default sink to the bluez sink
-pacmd set-default-sink bluez_sink.E9_08_EF_56_12_64
-
-
-# Move all currently playing audio streams to sink
-
-# List all current streams
-pacmd list-sink-inputs | grep "index:" | awk  -F" " '{print $2}'
-
-# Move current streams to my sink
-pacmd move-sink-input [integer] bluez_sink.E9_08_EF_56_12_64
+if [[ "$1" -eq "mpow" ]]; then
+	use_mpow
+elif [[ "$1" -eq "speakers"]]; then
+	use_speakers
+fi
